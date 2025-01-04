d1da8a69634d134ef0d754b7f4e15b7d7060d0e5
ny9u <604098901@qq.com>
2025-01-04 22:42:55 +0800

    手写lru

--- practice.js
+++ practice.js
@@ -287,3 +287,49 @@ Array.prototype.most = function(){
 	})
 	return element
 }
+
+// 手写lru
+/*
+lru:最近最少使用的元素
+*/
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+	this.cache = new Map()
+	this.capacity = capacity
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+	if(this.cache.has(key)){
+		let tmp = this.cache.get(key)
+		this.cache.delete(key)
+		this.cache.set(key,tmp)
+		return tmp
+	}
+	return -1
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  // if(this.cache.has(key)){
+	// 	this.cache.delete(key)
+	// }
+	// this.cache.set(key,value)
+	// 忘记对容量进行判断了,如果超过容量应该删除最少使用的元素
+	if(this.cache.has(key)){
+		this.cache.delete(key)
+	}else if(this.cache.size >= this.capacity){
+		//获取到最少使用的元素的key,keys()获取到key的迭代器
+		this.cache.delete(this.cache.keys().next().value)
+	}
+	this.cache.set(key,value)
+};
